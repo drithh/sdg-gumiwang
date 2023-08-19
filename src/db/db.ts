07a42cf84531eb5b5f92f266d7535861bc408275
drithh <adrielalfeus@gmail.com>
2023-08-19 11:30:22 +0700

    ignore env, add prisma

---
+++ src/db/db.ts
@@ -0,0 +1,9 @@
+// db.js
+import { drizzle } from 'drizzle-orm/planetscale-serverless';
+import { connect } from '@planetscale/database';
+
+export const connection = connect({
+  url: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(connection);
--- src/env.mjs
+++ src/env.mjs
@@ -5,13 +5,10 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
   },
-  client: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
-  },
+  client: {},
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   // experimental__runtimeEnv: {
